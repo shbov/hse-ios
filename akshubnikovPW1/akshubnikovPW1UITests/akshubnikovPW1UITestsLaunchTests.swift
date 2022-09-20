7cabcede73f33e210f7b38a84e7076156ac0d7ca
Andrew Shubnikov <a.shbov@yandex.ru>
2022-09-20 09:58:17 +0300

    add pw1

---
+++ akshubnikovPW1/akshubnikovPW1UITests/akshubnikovPW1UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  akshubnikovPW1UITestsLaunchTests.swift
+//  akshubnikovPW1UITests
+//
+//  Created by Andrew Shubnikov on 18.09.2022.
+//
+
+import XCTest
+
+final class akshubnikovPW1UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
